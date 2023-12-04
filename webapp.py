42220cb60376c22566cd1cc5ec4e5392a339e980
KiZers <clement.dumas.1@etsmtl.net>
2023-12-03 20:58:17 -0500

    Baselinde budget app

---
+++ webapp.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Liste des dépenses initiales
+expenses = [
+    {'id': 1, 'item': 'Pain', 'amount': 3.50},
+    {'id': 2, 'item': 'Lait', 'amount': 2.00},
+    {'id': 3, 'item': 'Œufs', 'amount': 4.25}
+]
+
+# Route pour afficher la liste des dépenses
+@app.route('/')
+def show_expenses():
+    return render_template('expenses.html', expenses=expenses)
+
+# Route pour ajouter une nouvelle dépense
+@app.route('/add', methods=['GET', 'POST'])
+def add_expense():
+    if request.method == 'POST':
+        item = request.form['item']
+        amount = float(request.form['amount'])
+        new_expense = {'id': len(expenses) + 1, 'item': item, 'amount': amount}
+        expenses.append(new_expense)
+        return redirect(url_for('show_expenses'))
+    return render_template('add_expense.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
